c9a3e8edbf484d2eb0c6a1a11f9f7a0d264938d5
RMulato07 <102757617+RMulato07@users.noreply.github.com>
2022-05-04 16:45:58 -0300

    Add files via upload

---
+++ exercicios.js
@@ -0,0 +1,34 @@
+//1-a
+console.log(20*30+10);
+
+//1-b
+console.log(4**2/30);
+
+//1-c
+console.log((9**4+2)*6-1);
+
+//2-a
+console.log(10 % 3* 10**1 + 1-10* 4 / 2);
+
+
+//3-a
+console.log('Ruan Mulato')
+
+//4-a
+const x = 3;
+const y = 5;
+
+console.log(2*x*3*y)
+
+//5-a
+const a = 5;
+const b = 17;
+const c = 9;
+
+console.log(a+b+c)
+
+//6-a
+const d = 1800;
+const e = 2;
+
+console.log(d*e)
